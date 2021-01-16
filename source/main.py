ce312eebe6b4a38defe22634f279d70cb0419ffb
Janik von Ahnen <j.v.ahnen@gmx.de>
2021-01-16 15:53:24 +0100

    update with drop down menu

--- source/main.py
+++ source/main.py
@@ -16,7 +16,7 @@ import easyocr
 import subprocess
 import webbrowser
 
-GIT_REPO_DIR=Path(__file__).absolute().parent.parent
+GIT_REPO_DIR = Path(__file__).absolute().parent.parent
 
 # fmt: off
 def comandline_argument_parser(parser=None):
@@ -25,31 +25,41 @@ def comandline_argument_parser(parser=None):
     return parser
 # fmt: on
 
+
 def clearScreen(frame):
     for widget in frame.winfo_children():
         widget.destroy()
 
-class OCRReader():
+
+class OCRReader:
     sim_reader = easyocr.Reader(["ch_sim", "en"])
     tra_reader = easyocr.Reader(["ch_tra", "en"])
 
-    def __init__(self):
-        self.reader=OCRReader.sim_reader
+    reader_modes = {"simplified": sim_reader, "traditional": tra_reader}
 
-    def read_image(self, file_path):
-        return "".join(self.reader.readtext(file_path, detail=0))
+    def __init__(self):
+        pass
+
+    def read_image(self, file_path, reader="simplified"):
+        return "".join(OCRReader.reader_modes[reader].readtext(file_path, detail=0))
+
 
 class Application(tk.Frame):
 
+    translation_provider_url = {
+        "Google": "https://translate.google.com/?hl=de&sl=auto&tl=en&text=TRANSLATETHIS&op=translate",
+        "MDBG": "https://www.mdbg.net/chinese/dictionary?page=worddict&wdrst=0&wdqtm=0&wdqcham=1&wdqt=TRANSLATETHIS",
+        "DeepL": "https://www.deepl.com/translator#zh/de/TRANSLATETHIS%0A",
+    }
+
     def __init__(self, master=None):
         super().__init__(master)
-        self.OCR_obj=OCRReader()
+        self.OCR_obj = OCRReader()
         self.master = master
         self.grid()
-        self.data_directory=self.create_data_directory("tmp_data")
+        self.data_directory = self.create_data_directory("tmp_data")
         self.create_widgets()
-        self.website="google"
-        self.current_OCR_text=None
+        self.current_OCR_text = None
 
         self.master.bind("q", lambda event: self.master.destroy())
         self.master.bind("s", lambda event: self.take_screenshot())
@@ -59,49 +69,56 @@ class Application(tk.Frame):
         self.master.bind("p", lambda event: self.switch_website())
 
     def create_data_directory(self, directory_name):
-        full_dir_name=os.path.join(GIT_REPO_DIR, directory_name)
+        full_dir_name = os.path.join(GIT_REPO_DIR, directory_name)
         os.makedirs(full_dir_name, exist_ok=True)
         return full_dir_name
 
     def switch_website(self):
-        if self.website=="google":
-            self.website="mdbg"
-            self.switch_website_button["text"]="Switch from mdbg to google translation (p)"
-        elif self.website=="mdbg":
-            self.website="google"
-            self.switch_website_button["text"]="Switch from google to mdbg translation (p)"
+        if self.website == "google":
+            self.website = "mdbg"
+            self.switch_website_button[
+                "text"
+            ] = "Switch from mdbg to google translation (p)"
+        elif self.website == "mdbg":
+            self.website = "google"
+            self.switch_website_button[
+                "text"
+            ] = "Switch from google to mdbg translation (p)"
         else:
             print("WARNING: this makes no sense!")
 
     def switch_reader(self):
         if self.OCR_obj.reader is OCRReader.sim_reader:
-            self.OCR_obj.reader=OCRReader.tra_reader
+            self.OCR_obj.reader = OCRReader.tra_reader
             self.switch_reader_button["text"] = "Switch to simplified Chinese OCR (c)"
         elif self.OCR_obj.reader is OCRReader.tra_reader:
-            self.OCR_obj.reader=OCRReader.sim_reader
+            self.OCR_obj.reader = OCRReader.sim_reader
             self.switch_reader_button["text"] = "Switch to traditional Chinese OCR (c)"
         else:
             print("WARNING: this makes no sense!")
 
     def update_text(self, text):
         self.Text.delete("1.0", "end")
-        self.Text.insert(tk.END, "\n"+text)
+        self.Text.insert(tk.END, "\n" + text)
         self.Text.tag_add("center", 1.0, "end")
         self.Text.update()
 
     def take_screenshot(self):
-        screenshot_file_path=os.path.join(self.data_directory, "screenshot.png")
-        subprocess.call(["import",screenshot_file_path])
-        self.current_OCR_text=self.OCR_obj.read_image(screenshot_file_path)
+        screenshot_file_path = os.path.join(self.data_directory, "screenshot.png")
+        subprocess.call(["import", screenshot_file_path])
+        self.current_OCR_text = self.OCR_obj.read_image(
+            screenshot_file_path, self.reader_mode.get()
+        )
         if self.current_OCR_text:
             pyperclip.copy(self.current_OCR_text)
             self.update_text(self.current_OCR_text)
 
     def translate(self):
-        if self.website=="google":
-            webbrowser.open(f"https://translate.google.com/?hl=de&sl=auto&tl=en&text={self.current_OCR_text}&op=translate")
-        elif self.website=="mdbg":
-            webbrowser.open(f"https://www.mdbg.net/chinese/dictionary?page=worddict&wdrst=0&wdqtm=0&wdqcham=1&wdqt={self.current_OCR_text}")
+        webbrowser.open(
+            Application.translation_provider_url[
+                self.translation_provider.get()
+            ].replace("TRANSLATETHIS", self.current_OCR_text)
+        )
 
     def quick_translate(self):
         self.take_screenshot()
@@ -109,15 +126,25 @@ class Application(tk.Frame):
 
     def create_widgets(self):
 
-        self.switch_reader_button = tk.Button(self)
-        self.switch_reader_button["text"] = "Switch to traditional Chinese OCR (c)"
-        self.switch_reader_button["command"] = self.switch_reader
-        self.switch_reader_button.grid()
+        self.translation_provider = tk.StringVar(self)
+        self.translation_provider.set(
+            list(Application.translation_provider_url.keys())[0]
+        )
+        self.translation_provider_menu = tk.OptionMenu(
+            self,
+            self.translation_provider,
+            *Application.translation_provider_url.keys()
+        )
+        self.translation_provider_menu.config(font=("Helvetica", 12))
+        self.translation_provider_menu.grid()
 
-        self.switch_website_button = tk.Button(self)
-        self.switch_website_button["text"] = "Switch from google to mdbg translation (p)"
-        self.switch_website_button["command"] = self.switch_website
-        self.switch_website_button.grid()
+        self.reader_mode = tk.StringVar(self)
+        self.reader_mode.set(list(OCRReader.reader_modes.keys())[0])
+        self.reader_mode_menu = tk.OptionMenu(
+            self, self.reader_mode, *OCRReader.reader_modes.keys()
+        )
+        self.reader_mode_menu.config(font=("Helvetica", 12))
+        self.reader_mode_menu.grid()
 
         self.take_screenshot_button = tk.Button(self)
         self.take_screenshot_button["text"] = "Take a Screenshot (s)"
@@ -134,12 +161,10 @@ class Application(tk.Frame):
         self.quick_translate_button["command"] = self.quick_translate
         self.quick_translate_button.grid()
 
-
-
         # TODO chinese characters are cut of at the top atm
-        self.Text = tk.Text(self,font=("Courier", 24), height=10, width=50)
+        self.Text = tk.Text(self, font=("Courier", 24), height=10, width=50)
         self.Text.grid()
-        self.Text.tag_configure("center", justify='center')
+        self.Text.tag_configure("center", justify="center")
         self.Text.insert(tk.END, "\nNo characters caputred so far!")
         self.Text.tag_add("center", 1.0, "end")
 
@@ -147,6 +172,7 @@ class Application(tk.Frame):
             self, text="QUIT (q)", fg="red", command=self.master.destroy
         )
         self.quit.grid()
+
 
 def main(args):
     root = tk.Tk()
